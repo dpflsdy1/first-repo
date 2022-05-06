969b6cad475a5a4acb53e279fd57715cc24ff201
yerin <dpflsdy1@icloud.com>
2022-05-06 13:28:34 +0900

    Modified ; to :

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('hello')
+#과제 1부터 10까지 반복하는 과정에서 3의 배수일경우, year 출력 나머지는 그대로 출력
+
+for i in range(1,20+1):
+    if i % 3 == 0:
+        print('year')
+    elif i % 5 == 0:
+        print('dream')
+    elif i %3 == 0 and i % 5==0:
+        print('yeardream')
+    else:
+        print(i)
